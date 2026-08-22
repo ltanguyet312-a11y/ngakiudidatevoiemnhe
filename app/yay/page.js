7de0c6191b8240198e017c62c1832fe6acf3fc59
ltanguyet312-a11y <lta.nguyet312@gmail.com>
2026-08-22 20:27:42 +0700

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,22 +3,19 @@
 export default function Yay() {
   return (
     <div className='box'>
-      {/* GIF 1: Gumball & Darwin */}
-      <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      
-      {/* https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTczMmlqOTBxYXAyZTI4Mmp3dXJwcnV6amVpNHZ2cmdkaHJpcGZybCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/g5FxC58T8Q7e0/giphy.gif */}
+      {/* GIF từ Giphy bạn gửi */}
       <img 
-        alt='cute gif' 
+        alt='Happy Birthday' 
         className='gif' 
-        src='https://media.tenor.com/gU812X4spRAAAAAi/dance-bear.gif' 
+        src='https://media.giphy.com/media/g5FxC58T8Q7e0/giphy.gif' 
       />
 
-      <h2>Yayyy~ Chốt lịch ngày đặc biệt với người đặc biệt nhé! 🥳💖</h2>
+      <h2>Yayyy~ Chốt ngày đặc biệt với người đặc biệt nhé! 🥳💖</h2>
 
       <div className="schedule">
         <div className="schedule-item">
           <span>📸</span>
-          <div><b>18:00 - 18:45:</b> Đi chụp photobooth </div>
+          <div><b>18:00 - 18:45:</b> Đi chụp photobooth xinh xẻo</div>
         </div>
         <div className="schedule-item">
           <span>🍕</span>
@@ -27,7 +24,7 @@ export default function Yay() {
       </div>
 
       <p className="note">
-        Mặc váy vào đi, tới giờ em đón đi choiii! iuuu &lt;3
+        Mặc váy xinh vào đi, tới giờ em đón đi choiii! iuuu &lt;3
       </p>
     </div>
   );
