d56f7d412a35ea1de2ea6989abcc4dd53d7dd6e4
ltanguyet312-a11y <lta.nguyet312@gmail.com>
2026-08-22 20:28:59 +0700

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,19 +3,19 @@
 export default function Yay() {
   return (
     <div className='box'>
-      {/* GIF từ Giphy bạn gửi */}
+      {/* GIF Selena Happy Birthday từ Giphy */}
       <img 
         alt='Happy Birthday' 
         className='gif' 
-        src='https://media.giphy.com/media/g5FxC58T8Q7e0/giphy.gif' 
+        src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3A1dWF1NG9tMTB4NmlreTRzbXp4NmVsOHB5OHZlOGQ0bzRybTNpZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g5FxC58T8Q7e0/giphy.gif' 
       />
 
-      <h2>Yayyy~ Chốt ngày đặc biệt với người đặc biệt nhé! 🥳💖</h2>
+      <h2>Yayyy~ Chốt lịch ngày đặc biệt với người đặc biệt nhé! 🥳💖</h2>
 
       <div className="schedule">
         <div className="schedule-item">
           <span>📸</span>
-          <div><b>18:00 - 18:45:</b> Đi chụp photobooth xinh xẻo</div>
+          <div><b>18:00 - 18:45:</b> Đi chụp photobooth </div>
         </div>
         <div className="schedule-item">
           <span>🍕</span>
