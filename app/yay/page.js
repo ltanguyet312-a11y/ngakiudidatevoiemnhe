c93c0c79bd1d5dda9ed011a359e70d9028d51cfd
ltanguyet312-a11y <lta.nguyet312@gmail.com>
2026-08-22 19:48:03 +0700

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,7 +3,9 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'Yayyy~ lets plan a date <3'}
+      {'Yayyy~ hẹn chị 18:00 - 18:45, đi chụp photobooth <3'}
+      {'Vàaaa~ ăn tối 18:45 - 20:00 nhá, i luv u <3'}
+      {'Mặc váy vào đi, tới giờ anh đón đi chơi, ps: iu lắm ngakiu oi 😽😽 '}
     </div>
   )
 
