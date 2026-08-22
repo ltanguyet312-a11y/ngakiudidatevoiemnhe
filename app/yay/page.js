e14e8218dfe07cc7d161566f46eb58373c30b18b
ltanguyet312-a11y <lta.nguyet312@gmail.com>
2026-08-22 20:04:09 +0700

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,10 +3,16 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-       {'Yayyy~ Chốt lịch mừng sinh nhật chị yêu nè 🥳💖'}
-      {'📸 18:00 - 18:45: Đi chụp photobooth '}
-      {'🍕 18:45 - 20:00: Đi ăn tối món chị thích'}
-      {'Mặc váy vào đi, tới giờ em đón đi choiii! iuuu <3'}
+        <h2>Yayyy~ Chốt lịch mừng sinh nhật chị yêu nè 🥳💖</h2>
+      <p style={{ fontSize: '1.2rem', margin: '10px 0' }}>
+        📸 <b>18:00 - 18:45:</b> Đi chụp photobooth xinh xẻo
+      </p>
+      <p style={{ fontSize: '1.2rem', margin: '10px 0' }}>
+        🍕 <b>18:45 - 20:00:</b> Đi ăn tối món chị thích
+      </p>
+      <p style={{ fontStyle: 'italic', marginTop: '15px' }}>
+        Mặc váy vào đi, tới giờ em đón đi choiii! iuuu &lt;3
+      </p>
     </div>
   )
 
